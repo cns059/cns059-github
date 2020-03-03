1c8c97abc1729a6ee4828a4f238a8467cd75f3c9
chenns <chenns@newland.com.cn>
2020-03-03 17:07:05 +0800

    init project version

---
+++ src/main/java/TestSwitch.java
@@ -0,0 +1,19 @@
+/**
+ * @author chenningshui
+ * @date 2020/3/3 14:23
+ */
+public class TestSwitch {
+    public static void main(String[] args) {
+        String str="hello";
+        switch (str){
+            case "hello":
+                System.out.println("hello");
+                break;
+            case "world":
+                System.out.println("world");
+                break;
+            default:
+                break;
+        }
+    }
+}
