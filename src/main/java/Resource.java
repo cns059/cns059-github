ef38e286dd7ef0fa6ea9327fc03c66282743b7c0
chenns <chenns@newland.com.cn>
2020-03-04 14:47:16 +0800

    all

---
+++ src/main/java/Resource.java
@@ -0,0 +1,13 @@
+/**
+ * @author chenningshui
+ * @date 2020/2/25 16:11
+ */
+public class Resource /*implements AutoCloseable*/ {
+/*    @Override
+    public void close() throws Exception {
+        System.out.println("Resource is closed");
+    }
+    public void say() {
+        System.out.println("hello,Resource");
+    }*/
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,8 @@
+import lombok.Data;
+
+@Data
+public class Student {
+    private String name;
+    private String age;
+    private String gender;
+}
